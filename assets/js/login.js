bfe4d641bd8f17bee01252b0ec175a598f7f2e29
nguyen ngoc <nguyenngoc@gmail.com>
2021-07-08 21:39:38 +0700

    upadate

---
+++ assets/js/login.js
@@ -0,0 +1,19 @@
+var a=document.getElementById("show");
+a.onclick=function(){
+    document.getElementById("showmodal").style.visibility="visible";
+}
+
+var back=document.getElementById("back");
+back.onclick=function(){
+    document.getElementById("showmodal").style.visibility="hidden";
+}
+
+var login=document.getElementById("login");
+login.onclick=function(){
+    document.getElementById("showlogin").style.visibility="visible";
+}
+
+var backlogin=document.getElementById("backlogin")
+backlogin.onclick=function(){
+    document.getElementById("showlogin").style.visibility="hidden";
+}
